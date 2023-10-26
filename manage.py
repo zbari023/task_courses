17d0beb603dd3937a922be9ba6a0775c514ec37f
Ziad Bari <barry450643@gmail.com>
2023-10-26 23:47:24 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,10 +4,7 @@ import os
 import sys
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-    try:
+
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
